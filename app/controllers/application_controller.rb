0f226b8db0ad39624f995191a09a48acaaae43b5
sergiohc <sergioh.c@live.com>
2018-04-01 16:56:53 -0300

    set controller aplication 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
 
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
