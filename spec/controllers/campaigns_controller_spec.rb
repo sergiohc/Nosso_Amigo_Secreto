31c3900cc389c21e4e1e04939a17648d9416c2cb
sergiohc <sergioh.c@live.com>
2018-04-02 21:57:08 -0300

    adjust merge

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe CampaignsController, type: :controller do
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
   include Devise::Test::ControllerHelpers
 
   before(:each) do
