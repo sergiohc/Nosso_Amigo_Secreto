4bf503d571a538929e944a286414445119c24edc
sergiohc <sergioh.c@live.com>
2018-04-02 21:51:32 -0300

    adjust merge

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -1,14 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CampaignsController, type: :controller do
-<<<<<<< HEAD
-
   describe "GET #show" do
     it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
     end
-=======
   include Devise::Test::ControllerHelpers
 
   before(:each) do
@@ -17,47 +14,8 @@ RSpec.describe CampaignsController, type: :controller do
     @request.env["devise.mapping"] = Devise.mappings[:user]
     @current_user = FactoryBot.create(:user)
     sign_in @current_user
->>>>>>> master
   end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-<<<<<<< HEAD
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #raffle" do
-    it "returns http success" do
-      get :raffle
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-end
-=======
   describe "GET #show" do
 
     context "campaing exists" do
@@ -212,4 +170,4 @@ end
     end
   end
 end
->>>>>>> master
+
